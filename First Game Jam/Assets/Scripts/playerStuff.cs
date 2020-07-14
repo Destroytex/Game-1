242f52463daf9cba78a4b842772d5bf660dbbdc2
Destroytex <abdulazizjohnson13@gmail.com>
2020-07-13 19:59:19 -0500

    add project

---
+++ First Game Jam/Assets/Scripts/playerStuff.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerStuff : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("Enemy"))
+        {
+            TakeDamage(20);
+        }
+    
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+        if (currentHealth <= 0)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        Debug.Log("damage");
+
+    }
+}
